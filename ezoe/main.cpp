2911e2ccf0542f4e9894fda99f1778451daa50ee
c10t <c10t@fa.ke>
2019-08-31 23:53:27 +0900

    hitch-hike guide

--- ezoe/main.cpp
+++ ezoe/main.cpp
@@ -1,4 +1,21 @@
 int main()
 {
-  std::cout << "hello\n" ;
+  double height = 1.63;
+  double mass = 73.0;
+  double bmi = mass / (height * height);
+  std::cout << "BMI="s << bmi << "\n"s ;
+
+  auto status = []( double bmi )
+  {
+    if (bmi < 18.5)
+      return "Underweight.\n"s;
+    else if (bmi < 25.0)
+      return "Normal.\n"s;
+    else if (bmi < 30.0)
+      return "Overweight.\n"s;
+    else
+      return "Obese.\n"s;
+  };
+
+  std::cout << status(bmi);
 }
